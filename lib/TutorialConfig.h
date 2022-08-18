df10fcbe0fefa881d47ef8afeb8bcce7389c058e
bporter97 <bporter78745@gmail.com>
2022-08-18 10:19:54 -0500

    testing CMAKE

---
+++ lib/TutorialConfig.h
@@ -0,0 +1,7 @@
+// the configured options and settings for Tutorial
+#include <string>
+
+#define Tutorial_VERSION_MAJOR @Tutorial_VERSION_MAJOR
+#define Tutorial_VERSION_MINOR @Tutorial_VERSION_MINOR
+
+void test(std::string);
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include "../lib/TutorialConfig.h"
 
 using namespace std;
 
 int main(){
 
+    string hello = "Hello Function!";
+
     cout << "Hello World!" << endl;
 
+    test(hello);
+    
     return 0;
+}
+
+void test(string h){
+    cout << h << endl;
 }
