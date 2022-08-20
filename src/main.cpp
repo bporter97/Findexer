377525de240940b95e55068ada1e64d9b0f04cb0
bporter97 <bporter78745@gmail.com>
2022-08-20 17:38:32 -0500

    testing arguments

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,18 @@
 
 using namespace std;
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
     cout << "You have entered " << argc
          << " arguments:" << "\n";
   
     for (int i = 0; i < argc; ++i)
         cout << argv[i] << "\n";
-  
+
+    for (int i = 0; i < argc; ++i)
+        cout << *argv[i] << "\n";
+
+    
+
     return 0;
 }
