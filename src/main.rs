479705709fede692c1fc850601afc17692553393
bporter97 <bporter78745@gmail.com>
2022-08-22 10:06:20 -0500

    implementing parameter handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use std::env;
+use std::io::ErrorKind;
+
+/*use std::fs;
+use std::os::windows; */
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    let param1_result: &String = &args[1];
+    let file_path: &String = &args[2];
+    let param2_result: &String = &args[3];
+    let file_name: &String = &args[4];
+
+    println!("{}", param1); 
 }
