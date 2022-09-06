11c607c1b973364cd1eaee3ab28bc103eee058e2
bporter97 <bporter78745@gmail.com>
2022-09-06 13:49:00 -0500

    nothing new yet

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include<iostream>
+#include<filesystem>
 
 int main(int argc, char* argv[]){
 
-    for(int i = 0; i < argc; ++i){
+    /*for(int i = 0; i < argc; ++i){
         std::cout << argv[i] << std::endl;
-    }
+    }*/
+
+    const std::string& file = argv[1];
+
+    std::cout << file;
 
     return 0;
 }
