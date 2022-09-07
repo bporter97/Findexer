95f1a4af7b940b3066227e81b00cad81b5f52f8a
bporter97 <bporter78745@gmail.com>
2022-09-07 14:15:03 -0500

    running across compiler issue with <filesystem>

--- libs/File_Dir.hpp
+++ libs/File_Dir.hpp
@@ -1,13 +1,17 @@
-#ifndef FILE_DR_H
-#define FILE_DR_H
+#ifndef FILE_DIR_H
+#define FILE_DIR_H
 
 #include <iostream>
-#include <fstream>
-#include <cstdlib>
+//#include <fstream>
+//#include <cstdlib>
+#include <filesystem>
 
-class File_Dir{
+class File_Dir{ //class for handling files and directories
     public:
-        File_Dir()
+        File_Dir(const std::string);
+        File_Dir(const std::string, const std::string);
+
+    private:
 
 };
 
