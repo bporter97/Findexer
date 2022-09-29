aa95708241942d0821b2dda694ce7ea9aed5dae8
bporter97 <bporter78745@gmail.com>
2022-09-28 21:08:29 -0500

    getting back into the groove

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,31 @@
 #include<filesystem>
 //#include "../libs/File_Dir.hpp"
 
-std::string param1; 
-std::string param2;    
-std::string param3;
-std::string param4;
+/*std::string*/ char param1; 
+/*std::string*/ char param2;    
+/*std::string*/ char param3;
+/*std::string*/ char param4;
 
 int main(int argc, char* argv[]){
 
+    std::cout << argv[1] << std::endl
+    << argv[2] << std::endl
+    << argv[3] << std::endl
+    << argv[4] << std::endl
+    << *argv[1] << "\t" << &argv[1] << std::endl
+    << *argv[2] << "\t" << &argv[2] << std::endl
+    << *argv[3] << "\t" << &argv[3] << std::endl
+    << *argv[4] << "\t" << &argv[4] << std::endl;
+
     //switch statement for processing command arguments; starts at 2 since the program accepts findex.exe as the first arg 
-    switch (argc){
+    /*switch (argc){
         case 2: //only -h
             param1 = argv[1]; //-h
             if(param1 == "-h"){
                 std::cout << "\nUsage: findex [-f or -d] <path-to-directory or file name> [-f] <filename>\n" 
                     << std::endl << "-d: Specify directory to search\n"
                     << "-f: Specify file name\n" //as of right now, program will only search file name and not extenstion type
+                    // << "-a: Search entire drive"
                     << "-h: help\n" << std::endl;
 
                 std::cout << param1;
@@ -27,7 +37,7 @@ int main(int argc, char* argv[]){
 
             break;
 
-        case 3: //two args entered, can only be -f and <filename>
+        case 3: //two args entered, can only be -f/-a and <filename>
             param1 = argv[1]; //--f
             param2 = argv[2]; // file name
             if (param1 == "-f"){ // take param2, return path(s) file-name is located at
@@ -60,7 +70,7 @@ int main(int argc, char* argv[]){
     default:
         std::cout << "Could not execute. Use findex -h for usage directions" << std::endl;
         break;
-    }
+    }*/
 
     return 0;
 }
