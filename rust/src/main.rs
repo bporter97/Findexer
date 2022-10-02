4b1977c81e1b88444cb0eeaef0fb3c70cf3b45b3
bporter97 <bporter78745@gmail.com>
2022-10-02 17:14:57 -0500

    update rust code

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -6,19 +6,22 @@ fn main() {
 
     match argc {
         //as of right now, program will only search file name and not extension
-        2 => {
-            if args[1] == "-h" {
-                println!(
-                    "\nUsage: findex [-f or -d] <path-to-directory or file name> [-f] <filename>\n
+        2 => if args[1] == "-h" {
+            println!("\nUsage: findex [-f or -d] <path-to-directory or file name> [-f] <filename>\n
             \n -d: Specify directory to search\n
             -f: Specify file name\n  
             -a: Search entire drive starting at root
-            -h: help\n"
-                );
+            -h: help\n");
             }
+        
+        3 => if args[1] == "-f" {
+            
+        } else if args[1] == "-a" {
+         
+        } else {
+            
         }
-
-        3 => if args[1] == "-f" {},
+    },
         _ => println!("Could not execute. Use findex -h for usage directions\n"),
     }
     println!("{}", args[1]);
